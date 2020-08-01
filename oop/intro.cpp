e98803f41bb224cada14e315e6c573a7248dade1
yashjsalian23 <yashjsalian23@gmail.com>
2020-08-01 15:52:49 +0530

    added function calls

--- oop/intro.cpp
+++ oop/intro.cpp
@@ -5,12 +5,17 @@ using namespace std;
 
 class Player{
     //attributes
-    string name;
-    int age;
+    public:
+        string name;
+        int age;
 
     //func definitions
-    void dispName(string s);
-    void dispAge(int n);
+    void dispName(){
+        cout<<name<<endl;
+    }
+    void dispAge(){
+        cout<<age<<endl;
+    }
 };
 
 int main(){
@@ -19,6 +24,15 @@ int main(){
 
     Player *ptr = new Player(); //dynamically creating class on the heap
     Player arr[] {yash, salian}; //array of obj 
+    yash.name = "Yash";
+    yash.age = 21;
+    yash.dispName();
+    yash.dispAge();
+
+    ptr->name = "ptr name";
+    ptr->age = 4;
+    ptr->dispAge();
+    ptr->dispName();
 
     return 0;
 }
