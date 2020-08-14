e4d42a276cfbc4280e2545f63c009b593aa0f393
yashjsalian23 <yashjsalian23@gmail.com>
2020-08-14 13:13:55 +0530

    added more efficient for valid palin

--- leetcode/validPalindrome.cpp
+++ leetcode/validPalindrome.cpp
@@ -14,7 +14,7 @@ public:
         string st = "";
         //cout<<s<<endl;
         for(i=0;i<len;i++){
-            if((s[i]>='a' && s[i]<='z') || s[i]>='0' && s[i]<='9'){
+            if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')){
                 st+=s[i];
             }
         }
@@ -24,6 +24,30 @@ public:
                 return false;
             }
         }
+        return true;
+    }
+
+     bool isPalindromeOpti(string s) {
+        int len = s.length();
+        if(len == 0){return true;}
+        
+        int i=0,j=len-1;
+        while(i<j){
+            //cout<<i<<" ";
+            if(!isalnum(s[i]) && !isalnum(s[j])){
+                i++;j--;
+            } else if(!isalnum(s[i])){
+                i++;
+            } else if(!isalnum(s[j])){
+                j--;
+            } else if(tolower(s[i++])!=tolower(s[j--])){
+                //cout<<i<<" "<<j<<" "<<len;
+                return false;
+            }
+            //i++;j--;
+           // cout<<i<<endl;
+        }
+        //cout<<i<<" "<<j<<" "<<len;
         return true;
     }
 };
